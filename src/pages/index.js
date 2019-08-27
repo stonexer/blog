51aecb070501b12425876be68e82b026663e37a6
SToneX <shitianxin@gmail.com>
2019-08-27 21:12:35 +0800

    Update components style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,56 +4,28 @@ import Particles from "react-particles-js"
 import particlesConfig from "../utils/particlesConfig"
 import SpringCard from "../components/springCard"
 
+import * as styles from "./index.module.css"
+
 function Home() {
-  const [height, setHeight] = React.useState(1000);
-  
+  const [height, setHeight] = React.useState(1000)
+
   React.useEffect(() => {
     if (window) {
-      setHeight(window.innerHeight);
+      setHeight(window.innerHeight)
     }
   }, [])
-  
-  return (
-    <>
-      <style>
-        {`
-          * {
-            box-sizing: border-box;
-          }
 
-          html,
-          body {
-            height: 100%;
-            padding: 0;
-            margin: 0;
-            font-family: 'avenir next', avenir, -apple-system, BlinkMacSystemFont, 'helvetica neue', helvetica,
-              ubuntu, roboto, noto, 'segoe ui', arial, sans-serif;
-            overflow: hidden;
-          }
-        `}
-      </style>
-      <div
-        style={{
-          position: "relative",
-          backgroundColor: "#1e282f",
-          padding: 0,
-          margin: 0,
-          overflow: "hidden",
-          boxSizing: "border-box",
-          fontFamily:
-            "avenir next,avenir,-apple-system,BlinkMacSystemFont,helvetica neue,helvetica,ubuntu,roboto,noto,segoe ui,arial,sans-serif",
-        }}
-      >
-        <Particles
-          params={particlesConfig}
-          height={height}
-          style={{ display: "block", transform: "translateZ(-600px)" }}
-        />
-        <div className="content">
-          <SpringCard />
-        </div>
+  return (
+    <div className={styles.container}>
+      <Particles
+        params={particlesConfig}
+        height={height}
+        style={{ display: "block", transform: "translateZ(-600px)" }}
+      />
+      <div className="content">
+        <SpringCard />
       </div>
-    </>
+    </div>
   )
 }
 
